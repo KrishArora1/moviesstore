ec2ec1be2252d5f904caf6d8dfae79ac77586cd4
Krish Arora <krisharora@lawn-128-61-15-13.lawn.gatech.edu>
2025-02-12 23:25:36 -0500

    first version

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "home",
     "movies",
+    "accounts",
+    "cart"
 ]
 
 MIDDLEWARE = [
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/New_York"
 
 USE_I18N = True
 
