445723aacff53cc6be71b8042982b26d0eb05869
Krish Arora <krisharora@Krishs-MacBook-Air-6.local>
2025-02-16 18:40:28 -0500

    Final Version

--- accounts/views.py
+++ accounts/views.py
@@ -5,9 +5,6 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
-from django.contrib.auth.forms import PasswordChangeForm
-
-
 # Create your views here.
 @login_required
 def logout(request):
